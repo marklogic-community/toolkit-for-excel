c41941554e58da7f7df64dc40dcd7eee6217a7ec
Justin Makeig <jmpublic+github@makeig.com>
2008-10-02 23:30:19 +0000

    Added a _debug flag to enable debugging
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/MLOS/trunk/Office2007@54231 62cac252-8da6-4816-9e9d-6dc37b19578c

--- Addins/Word/Samples/js/debug.js
+++ Addins/Word/Samples/js/debug.js
@@ -1,50 +1,63 @@
-/* $Id: debug.js,v 1.2 2008-10-02 22:37:49 jmakeig Exp $ */
+/* $Id: debug.js,v 1.3 2008-10-02 23:30:18 jmakeig Exp $ */
 
-if(!window.console) {
-	window.console = (function() {
-		var log = document.createElement("textarea");
-		log.id = "ML-Debug-Log";
-		log.readOnly = "readOnly";
-		return {
-			init: function() {
-				document.body.appendChild(log);
-				log = $(log);
-				log.setStyle({
-					"position": "fixed",
-					"bottom": "2em",
-					"left": "0",
-					"width": "20em",
-					"height": "10em",
-				    "z-index": "10"
-				});
-			},
-			log: function(message) {
-				log.value = message + "\n" + log.value; 
-			}
-		}
-	})();
-}
-var _l = window.console.log;
+// Last resort error handler
 Event.observe(window, "error", function(ex) {
-	alert(ex.message);
+	alert(ex);
 });
 
-document.observe("dom:loaded", function() {
-	window.console.init();
-	// Create a "Refresh" button
-	var refresh = document.createElement("button");
-	refresh.id = "ML-Debug-Refresh";
-	refresh.appendChild(document.createTextNode("Refresh"));
-	document.body.appendChild(refresh);
-	refresh = $(refresh); 
-	refresh.setStyle({
-		"position": "fixed",
-		"bottom": "2em",
-		"right": "0",
-		"width": "8em",
-	    "z-index": "10"
+
+// Global flag to enable debugging
+var _debug = true;
+
+// Global log function. By default it does nothing.
+var _l = function() {}
+
+if(_debug) {
+
+	if(!window.console) {
+		window.console = (function() {
+			var log = document.createElement("textarea");
+			log.id = "ML-Debug-Log";
+			log.readOnly = "readOnly";
+			return {
+				init: function() {
+					document.body.appendChild(log);
+					log = $(log);
+					log.setStyle({
+						"position": "fixed",
+						"bottom": "2em",
+						"left": "0",
+						"width": "20em",
+						"height": "10em",
+					    "z-index": "10"
+					});
+				},
+				log: function(message) {
+					log.value = message + "\n" + log.value; 
+				}
+			}
+		})();
+	}
+	_l = window.console.log;
+
+
+	document.observe("dom:loaded", function() {
+		window.console.init();
+		// Create a "Refresh" button
+		var refresh = document.createElement("button");
+		refresh.id = "ML-Debug-Refresh";
+		refresh.appendChild(document.createTextNode("Refresh"));
+		document.body.appendChild(refresh);
+		refresh = $(refresh); 
+		refresh.setStyle({
+			"position": "fixed",
+			"bottom": "2em",
+			"right": "0",
+			"width": "8em",
+		    "z-index": "10"
+		});
+		refresh.observe("click", function(){ 
+				window.location.reload();
+		});
 	});
-	refresh.observe("click", function(){ 
-			window.location.reload();
-	});
-});
+}
