8b475c63ee873ae6cdf2ede516200deeb3aef226
Pete Aven <wpaven@yahoo.com>
2010-09-27 20:54:17 +0000

    MarkLogic Sample Authoring App for PowerPoint 1.0-1
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/MLOS/trunk/Office2007@54740 62cac252-8da6-4816-9e9d-6dc37b19578c

---
+++ Addins/Samples/PowerPoint/pptAuthor/js/MarkLogicPowerPointEventSupport.js
@@ -0,0 +1,340 @@
+/* 
+Copyright 2008-2010 MarkLogic Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+MarkLogicPowerPointEventSupport.js - javascript api captures events in PowerPoint.
+
+for events you'd like to use, add try/catch and handler call
+*/
+
+var debug = false;
+
+windowSelectionChange  = function(shapename)
+{
+	//alert("windowSelectionChange: shapename "+shapename);
+	try{
+
+	var msg = shapename;
+	windowSelectionHandler(shapename);
+	}
+	catch(err)
+ 	{
+		msg="error in windowSelectionChange: "+err.description;
+	}
+
+	return msg;
+}
+
+windowBeforeRightClick  = function(slideIndex)
+{
+	try{ 
+		var msg = slideIndex;
+		if(debug)
+	           alert("windowBeforeRightClick: slideIndex "+slideIndex);
+	}
+	catch(err)
+ 	{
+		msg="error in windowBeforeRightClick: "+err.description;
+	}
+
+	return msg;
+}
+
+windowBeforeDoubleClick  = function(slideIndex)
+{
+	try{
+		var msg = slideIndex;
+	        if(debug)
+	          alert("windowBeforeDoubleClick: slideIndex "+slideIndex);
+	}
+	catch(err)
+ 	{
+		msg="error in windowBeforeDoubleClick: "+err.description;
+	}
+
+	return msg;
+}
+
+presentationClose  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("presentationClose: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationClose: "+err.description;
+	}
+
+	return msg;
+}
+
+presentationSave  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("presentationSave: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationSave: "+err.description;
+	}
+
+	return msg;
+}
+
+presentationOpen  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("presentationOpen: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationOpen: "+err.description;
+	}
+
+	return msg;
+}
+
+newPresentation  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("newPresentation: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in newPresentation: "+err.description;
+	}
+
+	return msg;
+}
+
+presentationNewSlide  = function(slideIndex)
+{
+	try{
+		if(debug)
+	           alert("presentationNewSlide: slideIndex "+slideIndex);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationNewSlide: "+err.description;
+	}
+
+	return msg;
+}
+
+windowActivate  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("windowActivate: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in windowActivate: "+err.description;
+	}
+
+	return msg;
+}
+
+windowDeactivate  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("windowDeactivate: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in windowDeactivate: "+err.description;
+	}
+
+	return msg;
+}
+
+slideShowBegin  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("slideShowBegin: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in slideShowBegin: "+err.description;
+	}
+
+	return msg;
+}
+
+slideShowNextBuild  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("slideShowNextBuild: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in slideShowNextBuild: "+err.description;
+	}
+
+	return msg;
+}
+
+slideShowNextSlide  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("slideShowNextSlide: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in slideShowNextSlide: "+err.description;
+	}
+
+	return msg;
+}
+
+slideShowEnd  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("slideShowEnd: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in slideShowEnd: "+err.description;
+	}
+
+	return msg;
+}
+
+presentationPrint  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("presentationPrint : slideIndex "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationPrint : "+err.description;
+	}
+
+	return msg;
+}
+/*
+ windowBeforeRightClick()
+windowBeforeDoubleClick()
+presentationClose()
+presentationSave()
+presentationOpen()
+newPresentation()
+presentationNewSlide()
+windowActivate()
+windowDeactivate()
+slideShowBegin()
+slideShowNextBuild()
+slideShowNextSlide()
+slideShowEnd()
+presentationPrint()
+*/
+
+
+slideSelectionChange = function(slideIndex)
+{
+	/*try
+	{
+	   var msg = slideIndex;
+           alert("slideSelectionChange: "+msg);
+	   var slide_tags = MLA.getSlideTags(msg);
+
+	   for(var i =0;i<slide_tags.tags.length;i++)
+	   {
+		var tag = slide_tags.tags[i];
+		alert("TagName: "+tag.name+" TagValue: "+tag.value);
+	   }
+        }
+	catch(err)
+ 	{
+		msg="error in slideSelectionChange: "+err.description;
+		//alert(msg);
+	}
+
+	return msg;*/
+
+	try
+	{
+	   slideSelectionHandler(slideIndex);
+	}
+	catch(err)
+ 	{
+	    msg="error in slideSelectionChange: "+err.description;
+	}
+
+}
+
+
+/*
+windowSelectionChange()
+
+slideSelectionChanged()
+colorSchemeChanged()
+presentationBeforeSave()
+slideShowNextClick()
+
+   */
+
+colorSchemeChange  = function(shapeRangeName)
+{
+	try{
+		if(debug)
+	           alert("colorSchemeChange : shapeRangeName "+shapeRangeName);
+	}
+	catch(err)
+ 	{
+		msg="error in colorSchemeChange : "+err.description;
+	}
+
+	return msg;
+}
+
+presentationBeforeSave  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("presentationBeforeSave : presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in presentationBeforeSave : "+err.description;
+	}
+
+	return msg;
+}
+
+slideShowNextClick  = function(presoName)
+{
+	try{
+		if(debug)
+	           alert("slideShowNextClick: presoName "+presoName);
+	}
+	catch(err)
+ 	{
+		msg="error in slideShowNextClick: "+err.description;
+	}
+
+	return msg;
+}
+
+
---
+++ Addins/Samples/PowerPoint/pptAuthor/search/insert-slide.xqy
@@ -0,0 +1,40 @@
+xquery version "1.0-ml";
+(:
+Copyright 2008-2010 Mark Logic Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+:)
+
+declare namespace search="http://marklogic.com/openxml/search";
+declare namespace w    ="http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+declare namespace q    ="http://marklogic.com/beta/searchbox";
+declare namespace xlink="http://www.w3.org/1999/xlink";
+declare namespace pkg="http://schemas.microsoft.com/office/2006/xmlPackage";
+declare namespace ins="http://marklogic.com/openxml/insert";
+declare namespace dc="http://purl.org/dc/elements/1.1/";
+declare namespace ppt=  "http://marklogic.com/openxml/powerpoint";
+
+let $uri := xdmp:get-request-field("uri")
+let $rId := xdmp:get-request-field("rid")
+
+let $doc-props := xdmp:document-properties($uri)
+let $custom-slide-metadata := $doc-props/prop:properties/ppt:slidetags/ppt:tag[@ppt:rid = $rId]/ppt:custompart
+let $custom-shape-metadata := $doc-props/prop:properties/ppt:shapetags/ppt:shape/ppt:tag/ppt:custompart
+
+let $metadata := for $meta in  ($custom-slide-metadata,$custom-shape-metadata)
+                 return <meta>{fn:doc($meta)}</meta>
+
+return xdmp:quote(<insertable>
+                     {$metadata}
+                  </insertable>)
+
