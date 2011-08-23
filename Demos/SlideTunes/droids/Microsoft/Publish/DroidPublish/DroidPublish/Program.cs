337e7acd67c80c46a32d0c344348404ea4e4b239
Pete Aven <wpaven@yahoo.com>
2011-08-23 19:52:50 +0000

    look sir, droids
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/integrations/Office/trunk@71315 62cac252-8da6-4816-9e9d-6dc37b19578c

---
+++ Demos/SlideTunes/droids/Microsoft/Publish/DroidPublish/DroidPublish/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marklogic.Xcc;
+
+namespace DroidPublish
+{ 
+    class Program
+    {
+        //public static Uri serverUri = new Uri("xcc://oslo:oslo@localhost:8032");
+        static void Main(string[] args)
+        {
+            Uri serverUri = new Uri("xcc://oslo:oslo@localhost:8032");
+            //Uri serverUri = new Uri("xcc://oslo:oslo@localhost:8032");
+            String galleryQueryUri = "gallery-query.xqy"; //args[1];
+
+            ModuleRunner queryRunner = new ModuleRunner(serverUri);
+            //String result = runner.InvokeToSingleString(moduleUri, "\n");
+            String[] result = queryRunner.InvokeToStringArray(galleryQueryUri);
+
+            int length = result.Length;
+
+            //if ( Int32.Parse(result) > 0)
+            if (length > 0)
+            {
+                Console.WriteLine("FILES FOUND: " + length);
+                for (int i = 0; i < length; i++)
+                {
+                    GalleryFactory gf = new GalleryFactory();
+                    Console.WriteLine("FILE NAME: " + result[i]);
+                    gf.publishGallery(result[i]);
+                }
+
+            }
+        }
+    }
+}
