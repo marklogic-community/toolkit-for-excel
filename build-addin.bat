4c083cefa595a4fae4c215b506d449d53a1740be
Dave Cassel <dmcassel@gmail.com>
2017-07-28 11:45:49 -0400

    changing Word references to Excel

--- build-addin.bat
+++ build-addin.bat
@@ -1,5 +1,4 @@
-rem cd C:\cygwin\home\paven\MLOS\Office2007\Addins\Word\Microsoft\MarkLogic_WordAddin\MarkLogic_WordAddin
-cd Addins\Word\Microsoft\MarkLogic_WordAddin\MarkLogic_WordAddin
+cd Addins\Excel\Microsoft\MarkLogic_ExcelAddin\MarkLogic_ExcelAddin
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 call  "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
